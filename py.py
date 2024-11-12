72071d62031479d85800d3cca3080f026a8de2d1
technicalxakash <akashr2468@gmail.com>
2024-11-12 23:22:43 +0530

    h5

--- py.py
+++ py.py
@@ -950,13 +950,81 @@ These arguments are used when a function can accept a variable number of keyword
 # # print the new string
 # print("The string after replacement of positions : " +new_str)
 # #This code is contributed by Jyothi pinjala.
-new_str = ''
-str1="apples"
-for char in str1:
-    if char == 'b':
-        new_str += 'a'
-    elif char == 'a':
-        new_str += 'b'
-    else:
-        new_str += char
-print(new_str)
+# new_str = ''
+# str1="apples"
+# for char in str1:
+#     if char == 'b':
+#         new_str += 'a'
+#     elif char == 'a':
+#         new_str += 'b'
+#     else:
+#         new_str += char
+# print(new_str)
+
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+# Example
+
+# The ordered list of scores is , so the second lowest score is . There are two students with that score: . Ordered alphabetically, the names are printed as:
+
+# alpha
+# beta
+# Input Format
+
+# The first line contains an integer, , the number of students.
+# The  subsequent lines describe each student over  lines.
+# - The first line contains a student's name.
+# - The second line contains their grade.
+
+# Constraints
+
+# There will always be one or more students having the second lowest grade.
+# Output Format
+
+# Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
+
+# Sample Input 0
+
+# 5
+# Harry
+# 37.21
+# Berry
+# 37.21
+# Tina
+# 37.2
+# Akriti
+# 41
+# Harsh
+# 39
+# Sample Output 0
+
+# Berry
+# Harry
+# Explanation 0
+
+# There are  students in this class whose names and grades are assembled to build the following list:
+
+# python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+# The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
+#solution 
+# if __name__ == '__main__':
+#     records=[]
+#     s=set()
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
+       
+#         records.append([name,score])
+#         s.add(score)
+#     sls=sorted(s)[1]
+#     sln=[]
+#     for name,score in records:
+#         if score==sls:
+#             sln.append(name)
+#     for name in sorted(sln):
+#         print(name)
+            
+        
